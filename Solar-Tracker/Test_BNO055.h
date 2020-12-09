8909c9b8f64862267e5ef06de4903787d9411913
stewathia3 <71801175+stewathia3@users.noreply.github.com>
2020-12-09 08:48:42 -0500

    Update Test_BNO055.h

--- Solar-Tracker/Test_BNO055.h
+++ Solar-Tracker/Test_BNO055.h
@@ -1,10 +1,13 @@
-
-/*************************************************************************
-* Title:    C include file for the I2C interface is included in i2c.c
-* Author:   Maitreyee Rao
-* Reference :  This file was built with reference to
-https://github.com/adafruit/Adafruit_BNO055/blob/master/Adafruit_BNO055.h
-**************************************************************************/
+/*
+ * Maitreyee Rao
+ * ECEN 5613 - Fall 2020 - Prof. McClure
+ * University of Colorado Boulder
+ * Revised 12/6/20
+ *
+ * C include file for the BNO005 interface is included in bno005.c
+ *
+ * Note: Referenced and modified from: https://github.com/adafruit/Adafruit_BNO055/blob/master/Adafruit_BNO055.h
+ */
 
 #ifndef TEST_BNO055_H_INCLUDED
 #define TEST_BNO055_H_INCLUDED
