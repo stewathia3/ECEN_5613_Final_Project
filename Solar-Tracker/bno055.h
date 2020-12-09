09234a6342a1af0248fc50f88c54f2159facb19a
stewathia3 <71801175+stewathia3@users.noreply.github.com>
2020-12-09 08:49:10 -0500

    Update bno055.h

--- Solar-Tracker/bno055.h
+++ Solar-Tracker/bno055.h
@@ -1,11 +1,13 @@
-/*************************************************************************
-* Title:    C include file for the bno055 interface is included in bno055.c
-* Author:   Maitreyee Rao
-* Reference : Bosch BNO055 datasheer
-**************************************************************************/
-
-
-
+/*
+ * Maitreyee Rao
+ * ECEN 5613 - Fall 2020 - Prof. McClure
+ * University of Colorado Boulder
+ * Revised 12/6/20
+ *
+ * C include file for the BNO005 interface is included in bno005.c
+ *
+ * Note: Referenced and modified from: https://github.com/adafruit/Adafruit_BNO055/blob/master/Adafruit_BNO055.h
+ */
 
 #ifndef BNO055_H_INCLUDED
 #define BNO055_H_INCLUDED
